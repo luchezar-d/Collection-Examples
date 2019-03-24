7734325322dfdebda0013d9dbbc2517ed422370b
Luchezar <Luchezar.d.dimitrov@gmail.com>
2019-03-25 00:43:41 +0200

    Added Hashtables

---
+++ Collections/Collections/Collections/HashTables.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections.Collections
+{
+    public class HashTables
+    {
+        public HashTables()
+        {
+            Console.WriteLine("Some examples of working with Hashtables");
+            Console.WriteLine("------------------------------------------");
+
+            Hashtable table = new Hashtable();
+
+            table.Add(100, "Peter");
+            table.Add(101, "Ivan");
+            table.Add(99, "Gosho");
+
+            //automatically sorting the items in descending order
+            foreach (int Id in table.Keys)
+            {
+                Console.WriteLine("Key : " + Id);
+            }
+            Console.WriteLine("-----------------");
+            foreach (string name in table.Values)
+            {
+                Console.WriteLine("Value : " + name);
+            }
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Item with key 101 : " + table[101]);
+        }
+    }
+}
--- Collections/Collections/Program.cs
+++ Collections/Collections/Program.cs
@@ -1,19 +1,27 @@
-﻿using System;
+﻿using Collections.Collections;
+using System;
 using System.Collections;
 
 namespace Collections
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
             ArrayLists lists = new ArrayLists();
             Console.WriteLine("--------------------------------------------------------------------------------");
+
             Dictionaries dictionaries = new Dictionaries();
             Console.WriteLine("--------------------------------------------------------------------------------");
+
             Queues queues = new Queues();
             Console.WriteLine("--------------------------------------------------------------------------------");
+
             Stacks stacks = new Stacks();
+            Console.WriteLine("--------------------------------------------------------------------------------");
+
+            HashTables tables = new HashTables();
+            Console.WriteLine("--------------------------------------------------------------------------------");
 
             Console.Read();
         }
