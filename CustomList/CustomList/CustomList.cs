97f3e78a361570dbd3f20333d382977fd234cb32
Luchezar <Luchezar.d.dimitrov@gmail.com>
2019-05-02 02:12:45 +0300

    adding CustomList.cs

---
+++ CustomList/CustomList/CustomList.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomList<T>
+    {
+        private T[] buffer;
+        private int size;
+        private const int MIN = 4;
+
+        public CustomList()
+        {
+            buffer = null;
+            size = 0;
+        }
+
+        public int Size => size;
+        public int Catapcity => buffer.Length;
+
+        public void AddToTheEnd(T value)
+        {
+            if (buffer == null)
+            {
+                buffer = new T[MIN];
+            }
+            else
+            {
+                if (size == buffer.Length)
+                {
+                    Expand(size * 2);
+                }
+            }
+            buffer[size] = value;
+            ++size;
+        }
+        public void Expand(int newSize)
+        {
+            if (newSize < size)
+            {
+                return;
+            }
+            var newBuffer = new T[newSize];
+
+            for (int i = 0; i < size; i++)
+            {
+                newBuffer[i] = buffer[i];
+            }
+
+            buffer = newBuffer;
+        }
+        public void RemoveFromTheEnd()
+        {
+            if (size == 0)
+            {
+                throw new IndexOutOfRangeException("Removing from empty list"); 
+            }
+            
+            --size;
+            buffer[size] = default(T);
+        }
+        public void ShrinkToFit()
+        {
+            Expand(size);
+        }
+
+        public void Clear()
+        {
+            size = 0;
+            buffer = new T[MIN];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return buffer[index];
+            }
+            set
+            {
+                buffer[index] = value;
+            }
+        }
+        public T Last
+        {
+            get
+            {
+                return buffer[size - 1];
+            }
+            set
+            {
+                buffer[size - 1] = value;
+            }
+        }
+        
+    }
+}
---
+++ CustomList/CustomList/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    class Program
+    {
+        public static void Time(Action func)
+        {
+            var watch = new Stopwatch();
+
+            watch.Start();
+            func();
+            watch.Stop();
+
+            Console.WriteLine($"Elapsed : {watch.Elapsed}");
+        }
+
+        static void Main(string[] args)
+        {
+            //var list = new CustomList<int>();
+            var list = new CustomList<CustomList<int>>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                var newList = new CustomList<int>();
+                newList.AddToTheEnd(4);
+                newList.AddToTheEnd(i);
+                list.AddToTheEnd(newList);
+            }
+            {
+                var lastElement = list.Last;
+                list.RemoveFromTheEnd();
+
+                for (int i = 0; i < 100000000; i++)
+                {
+                    lastElement.AddToTheEnd(i);
+                }
+            }
+
+            Console.Read();
+            //Time(() => {
+            //    list.Expand(100000000);
+
+            //    for (int i = 0; i < 100000001; i++)
+            //    {
+            //        list.AddToTheEnd(i);
+            //    }
+            //    //Console.WriteLine(list.Catapcity);
+
+            //    while (list.Size > 0)
+            //    {
+            //        list.RemoveFromTheEnd();
+            //    }
+            //    list.RemoveFromTheEnd();
+
+            //    Console.WriteLine($"Should be -1 {list.Size}");
+            //    list.AddToTheEnd(4);
+            //});
+
+        }
+        
+    }
+    
+}
